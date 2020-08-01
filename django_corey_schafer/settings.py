6ff80827c26a4bd8445da71867b9e6258fc4aa53
Parth Lashkari <parthlashkari111@gmail.com>
2020-08-01 08:37:15 +0530

    Updated Settings9

--- django_corey_schafer/settings.py
+++ django_corey_schafer/settings.py
@@ -63,6 +63,7 @@ AUTHENTICATION_BACKENDS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -152,6 +153,8 @@ STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 # ]
 
 STATIC_URL = '/static/'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 
